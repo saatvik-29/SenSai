c02afdc37a7269fdaf3aa8f790c32d60ffc38d3f
akshatsri47 <ak.sr2201@gmail.com>
2025-08-08 02:34:33 +0530

    test

--- sensai-frontend/src/app/layout.tsx
+++ sensai-frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "@/providers/SessionProvider";
-import VapiWidget from "@/hooks/Vapi";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
